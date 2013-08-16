9cb31ba3fe2d721bf0e893a024243e78d7ba73b3
dhiraj1125 <student_2@PC6>
2013-08-16 11:24:29 +0530

    n program for git

---
+++ n/src/n/Dollar.java
@@ -0,0 +1,11 @@
+package n;
+
+public class Dollar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is program for git understanding");
+	}
+
+}
